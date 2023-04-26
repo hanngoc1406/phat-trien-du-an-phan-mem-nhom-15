68c8afd3ebf50bc4ba55df61cc2045bd74a467fb
Phan Duc <gumball1506@gmail.com>
2023-04-26 23:18:19 +0700

    initial login view

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-var io = require('socket.io')(server);
 var express = require('express');
+const bodyParser = require('body-parser'); 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+var server = require('http').Server(app);
 app.use(express.static('./www'));
 
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+app.get('/login', (req, res) => {
+	res.sendFile(__dirname + '/www/login');
+});
+
+app.post('/auth', (req, res) => {
+	
+});
 
 // port
 server.listen(process.env.PORT || 8080, function(){
